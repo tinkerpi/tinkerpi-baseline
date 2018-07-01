006ad52f63ca2c1cd0227c9441932c6c97b0560f
Lory Livezey <lory@tinkerbase.com>
2018-07-01 19:55:04 -0400

    Changes to L298 Motor Driver MOD-0023

--- sensors/MOD-0023-L298N-Motor-Driver/MOD-0023-L298N-Motor-Driver.ino
+++ sensors/MOD-0023-L298N-Motor-Driver/MOD-0023-L298N-Motor-Driver.ino
@@ -1,20 +1,23 @@
-/* L298N DC motor example */
+
+/* Tinker Pi L298 Motor Driver - MOD-0023 */
 
 int IN1=8;
 int IN2=9;
 int ENA=3;
+
 void setup()
 {
- pinMode(IN1,OUTPUT);
- pinMode(IN2,OUTPUT);  
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);  
 }
+
 void loop()
 {  
- analogWrite(ENA, 200);// motor speed  
- digitalWrite(IN1,LOW);// rotate forward
- digitalWrite(IN2,HIGH);
- delay(2000);  
- digitalWrite(IN1,HIGH);// rotate reverse
- digitalWrite(IN2,LOW);
- delay(2000);
+  analogWrite(ENA, 200);    // motor speed  
+  digitalWrite(IN1,LOW);    // rotate forward
+  digitalWrite(IN2,HIGH);
+  delay(2000);  
+  digitalWrite(IN1,HIGH);   // rotate reverse
+  digitalWrite(IN2,LOW);
+  delay(2000);
 }
